40188ba93fc63c5064131a07fdd0148fbf37fbb6
Muu79 <muu794@gmail.com>
2023-05-29 17:17:42 +0200

    Added homework5

--- week1/Homework4/fizz_buzz/src/main.rs
+++ week1/Homework4/fizz_buzz/src/main.rs
@@ -12,4 +12,22 @@ fn main() {
         }
     }
     println!("\"fizz buzz\" was printed {} times", count)
+
+    //function to do the common fizz buzz problem involving modulo 5 and 3, but print the amount of times fizz buzz was printed and print fizz and buzz if the number is divisible by 3 or 5 repsectively
+    fn fizz_buzz(){
+        let mut count = 0;
+        for i in 0..=301{
+            if i % 5 == 0 && i % 3 == 0{
+                println!("fizz buzz");
+                count+=1;
+            }
+            else if i % 3 == 0{
+                println!("fizz");
+            }else if i % 5 == 0{
+                println!("buzz");
+            }
+        }
+        println!("\"fizz buzz\" was printed {} times", count)
+    }
+    
 }
