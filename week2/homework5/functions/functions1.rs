67d084f1a1fbba412445a9a20afa0b3c55a25981
Muu79 <muu794@gmail.com>
2023-05-30 19:59:14 +0200

    Fixed homework5 to have correct code

--- week2/homework5/functions/functions1.rs
+++ week2/homework5/functions/functions1.rs
@@ -1,8 +1,10 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
 
+fn call_me(){
+    println!("Hello World!");
+}
 
 fn main() {
     call_me();
